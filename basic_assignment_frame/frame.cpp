b744e2fe01858f0f63fd84813454b0a13395db43
JungMin An <qqqwkd@kookmin.ac.kr>
2018-05-08 13:19:28 +0900

    basic frame for assignment

---
+++ basic_assignment_frame/frame.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    ifstream inStream;
+    int numTestCases;
+    
+    inStream.open("input.txt");
+    if(inStream.fail())
+    {
+        cerr << "Input file opening failed.\n";
+        exit(1);
+    }
+    
+    inStream >> numTestCases;
+    
+    for (int i = 0; i<numTestCases; i++){
+    
+    }
+    inStream.close();
+}
